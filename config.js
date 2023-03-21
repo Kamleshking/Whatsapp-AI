2cb255e3c3f9b2a3c08b9dcea94acade266a7638
Kamleshking <101655383+Kamleshking@users.noreply.github.com>
2023-03-21 14:03:23 +0500

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const chalk = require('chalk')
 //============================================================( YOU CAN EDIT )====================================================================//
 
 
-global.openaikey = process.env.OPEN_AI_KEY || "ENTER_API-KEY_HERE", //Put Open AI Api key
-global.owner = process.env.OWNER || "94XXXXXXXX", //Bot Owner Number
+global.openaikey = process.env.OPEN_AI_KEY || "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", //Put Open AI Api key
+global.owner = process.env.OWNER || "923402725227", //Bot Owner Number
 global.ownername = process.env.OWNER_NAME || "DarkWinzo", //Bot Owner Name
 global.open_ai = process.env.OPEN_AI || "false", //Open AI Bot : "true" = on/"false" = off
 global.chatbot_pm = process.env.CHATBOT_PM || "true", //Inbox Msg Chat Bot : "true" = on/"false" = off
